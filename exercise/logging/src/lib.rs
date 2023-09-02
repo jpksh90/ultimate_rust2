eb2debd7011a31b184f0ff0e527893c478fd73cd
Jyoti Prakash <jpksh90@gmail.com>
2023-09-02 03:41:40 +0200

    changes

--- exercise/logging/src/lib.rs
+++ exercise/logging/src/lib.rs
@@ -1,5 +1,6 @@
 // 1. Bring the macros `error, warn, info, debug, trace` into scope from the log package with a
 // `use` statement.
+use log::{debug, error, info, trace, warn};
 //
 // You should be able to run `cargo build --lib` successfully after this step (and each step in this
 // file)
@@ -15,19 +16,24 @@ pub struct Frog {
 impl Frog {
     pub fn new() -> Self {
         // 2. Use debug!() to log "A new Frog has been created"
+        debug!("A new Frog has been created");
         Default::default()
     }
+
     pub fn hop(&mut self) {
         self.energy -= 1;
+        info!("a Frog hopped {}", self.energy);
         // 3. Use info!() to log that a Frog hopped, and how much energy is left
         if self.energy == 0 {
             // 4. Use warn!() to warn that the frog will go to sleep since he ran out of energy
+            warn!("flog will go to sleep since he ran out of energy");
             self.sleep();
         }
     }
     pub fn sleep(&mut self) {
         if self.sleeping {
             // 5. Use error!() to log a (non-fatal) error stating that the Frog is already asleep
+            error!("Flog is already asleep")
         } else {
             self.sleeping = true;
         }
@@ -37,6 +43,8 @@ impl Frog {
 impl Default for Frog {
     fn default() -> Self {
         // 6. Use trace!() to log that a default value was generated, with the debug representation
+        trace!("default vlue energy={} sleeping={}", 5, false);
+
         Frog {
             energy: 5,
             sleeping: false,
